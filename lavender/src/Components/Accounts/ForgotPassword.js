0cef41458c606bf74c326d7778ca666efc1870fc
unknown <vqh.official@gmail.com>
2021-12-17 20:13:47 +0700

    style for forgot password component

--- lavender/src/Components/Accounts/ForgotPassword.js
+++ lavender/src/Components/Accounts/ForgotPassword.js
@@ -1,22 +1,29 @@
 import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import axios from "axios";
 function ForgotPassword() {
     const [email, setEmail] = useState("");
     function saveUser() {
         let url = "https://localhost:5001/forgotpassword/" + email.toString();
         console.log(email);
-            axios.get(url)
+        axios.get(url);
     }
     return (
-        <form>
-            <div className="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" value={email} name="email" onChange={(e)=>{setEmail(e.target.value)}}
-                className="form-control" placeholder="Enter email" />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        <div className="row d-flex justify-content-center">
+            <div className="col-4">
+                <form>
+                    <div className="form-group ">
+                        <h3>Quên mật khẩu</h3>
+                        <input type="text"
+                            value={email} name="email"
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            className="form-control border mb-3" placeholder="Nhập Email hoặc tên đăng nhập" />
+                        <small id="emailHelp" className="form-text text-muted"></small>
+                    </div>
+                    <Link to={`https://mail.google.com/mail/u/?authuser=${email}`} className="btn btn-success">Lấy mật khẩu</Link>
+                </form>
             </div>
-            <button type="button" onClick={saveUser} className="btn btn-primary">Lấy Mật Khẩu</button>
-        </form>
+        </div>
     )
 }
 export default ForgotPassword;
