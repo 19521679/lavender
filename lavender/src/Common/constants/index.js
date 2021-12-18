1761395f71ee2862f65d9ce1084258bdd2ec272b
19521679 <19521679@gm.uit.edu.vn>
2021-12-18 17:27:49 +0700

    deploy front

--- lavender/src/Common/constants/index.js
+++ lavender/src/Common/constants/index.js
@@ -1,2 +1,2 @@
 export const API_ENDPOINT = "https://localhost:5001";
-export const CLIENT_ID = "https://19521679.github.io/lavender/";
+export const CLIENT_ID = "370364341020-9d1ctpi25g3i8snmm7lodl600gqphf3e.apps.googleusercontent.com";
