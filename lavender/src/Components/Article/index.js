59476c8ff3d6d04cb65f1126477e0efa9670547e
unknown <vqh.official@gmail.com>
2021-12-20 00:55:13 +0700

    fix comment on article detail

--- lavender/src/Components/Article/index.js
+++ lavender/src/Components/Article/index.js
@@ -62,8 +62,8 @@ function Article() {
                                                     >
                                                         {post.tieude}
                                                     </a></h5> */}
-                        <p className="post-description">{post.mota}</p>
-                      </div> 
+                        <h5> <a className="post-title" onClick={() => { window.location.href = `https://lavender-uit-webshop.web.app/articledetail/${post.mabaiviet}` }}>{post.tieude}</a></h5>
+                      </div>
                     </div>
                   ))}
                 </div>
