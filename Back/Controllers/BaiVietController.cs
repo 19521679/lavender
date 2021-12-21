25ed585d759a166190fc321ba0c204036db5f6d9
19521640 <68999238+huy164@users.noreply.github.com>
2021-12-21 23:00:08 +0700

    Update BaiVietController.cs

--- Back/Controllers/BaiVietController.cs
+++ Back/Controllers/BaiVietController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Logging;
 using Back.Models;
 using Newtonsoft.Json;
 using Microsoft.EntityFrameworkCore;
+using Back.ModelDTO;
+using Back.Models;
+
 
 namespace Back.Controllers
 {
