b276a49c60daee8becb8a538221731c25348c8b6
19521679 <19521679@gm.uit.edu.vn>
2021-12-22 02:26:19 +0700

    xuất file hoá đơn

--- Back/Controllers/ExcelController.cs
+++ Back/Controllers/ExcelController.cs
@@ -13,6 +13,7 @@ using System.Drawing;
 using OfficeOpenXml.Style;
 using System.ComponentModel;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Back.Controllers
@@ -22,15 +23,16 @@ namespace Back.Controllers
         lavenderContext lavenderContext;
         public ExcelController(lavenderContext lavenderContext)
         {
-            this.lavenderContext= lavenderContext;
+            this.lavenderContext = lavenderContext;
         }
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
-        [Route("/xuatfile")]
-        public async Task<IActionResult> XuatFile()
+        [Route("/xuat-hoadon")]
+        [Authorize(Roles = "ADMINISTRATOR, STAFF")]
+        public async Task<IActionResult> XuatHoadon(string tungay, string denngay)
         {
             ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
             var data = new DataTable();
@@ -44,105 +46,118 @@ namespace Back.Controllers
                 sheet.Column(1).Width = 5.33;
                 sheet.Column(2).Width = 11.67;
                 sheet.Column(5).Width = 20;
-                sheet.Cells["A1:C1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                sheet.Cells["A1:C1"].Value = "TRƯỜNG ĐH CÔNG NGHỆ THÔNG TIN";
-                sheet.Cells["A2:C2"].Merge = true;
-                sheet.Cells["A2:C2"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                sheet.Cells["A2:C2"].Style.Font.Bold = true;
-                sheet.Cells["A2:C2"].Value = "PHÒNG CÔNG TÁC SINH VIÊN";
-                sheet.Cells["A4:D4"].Merge = true;
-                sheet.Cells["A4:D4"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                sheet.Cells["A4:D4"].Style.Font.Bold = true;
-                sheet.Cells["A4:D4"].Style.Font.Size = 16;
-                sheet.Cells["A4:D4"].Value = "DANH SÁCH LỚP CVHT";
-                sheet.Cells["A5:C5"].Merge = true;
-                sheet.Cells["A5:C5"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-                sheet.Cells["A5:C5"].Style.Font.Bold = true;
-                sheet.Cells["A5:C5"].Value = "Khóa:";
-                sheet.Cells["D5:E5"].Merge = true;
-                sheet.Cells["D5:E5"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-                sheet.Cells["D5:E5"].Style.Font.Bold = true;
-                sheet.Cells["D5:E5"].Value = "";
-                sheet.Cells["A6:C6"].Merge = true;
-                sheet.Cells["A6:C6"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-                sheet.Cells["A6:C6"].Style.Font.Bold = true;
-                sheet.Cells["A6:C6"].Value = "Khoa:";
-                sheet.Cells["D6:E6"].Merge = true;
-                sheet.Cells["D6:E6"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-                sheet.Cells["D6:E6"].Style.Font.Bold = true;
-                sheet.Cells["D6:E6"].Value = "Lớp CVHT:";
-                sheet.Cells["A7:C7"].Merge = true;
-                sheet.Cells["A7:C7"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-                sheet.Cells["A7:C7"].Style.Font.Bold = true;
-                sheet.Cells["A7:C7"].Value = "Giảng viên CVHT:";
-                sheet.Cells["D7:E7"].Merge = true;
-                sheet.Cells["D7:E7"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-                sheet.Cells["D7:E7"].Style.Font.Bold = true;
-                sheet.Cells["D7:E7"].Value = "Mã giảng viên CVHT:";
+                sheet.Cells["A1:I1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                sheet.Cells["A1:A1"].Value = "TRƯỜNG ĐH CÔNG NGHỆ THÔNG TIN";
+                sheet.Cells["A1:I1"].Merge = true;
 
+                sheet.Cells["A2:I2"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                sheet.Cells["A2:I2"].Style.Font.Bold = true;
+                sheet.Cells["A2:A2"].Value = "LAVENDER UIT WEBSHOP";
+                sheet.Cells["A2:I2"].Merge = true;
 
+                sheet.Cells["A4:I4"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                sheet.Cells["A4:I4"].Style.Font.Bold = true;
+                sheet.Cells["A4:I4"].Style.Font.Size = 16;
+                sheet.Cells["A4:A4"].Value = "THỐNG KÊ DOANH SỐ BÁN SẢN PHẨM";
+                sheet.Cells["A4:I4"].Merge = true;
+
+                sheet.Cells["A5:I5"].Merge = true;
+                sheet.Cells["A5:I5"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                sheet.Cells["A5:I5"].Style.Font.Bold = true;
+                sheet.Cells["A5:A5"].Value = "Từ ngày:";
+
+                sheet.Cells["A6:I6"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                sheet.Cells["A6:I6"].Style.Font.Bold = true;
+                sheet.Cells["A6:A6"].Value = "Đến ngày:";
+                sheet.Cells["A6:I6"].Merge = true;
 
                 sheet.Row(9).Height = 41.13;
-                sheet.Cells["A9:D9"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                sheet.Cells["A9:D9"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
-                sheet.Cells["A9:D9"].Style.Fill.PatternType = ExcelFillStyle.Solid;
-                sheet.Cells["A9:D9"].Style.Fill.BackgroundColor.SetColor(0, 186, 248, 255);
-                sheet.Cells["A9:D9"].Style.Font.Bold = true;
-                sheet.Cells["A9:D9"].Style.Border.Left.Style = ExcelBorderStyle.Thin;
-                sheet.Cells["A9:D9"].Style.Border.Right.Style = ExcelBorderStyle.Thin;
-                sheet.Cells["A9:D9"].Style.Border.Top.Style = ExcelBorderStyle.Thin;
-                sheet.Cells["A9:D9"].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                sheet.Cells["A9:I9"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                sheet.Cells["A9:I9"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                sheet.Cells["A9:I9"].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                sheet.Cells["A9:I9"].Style.Fill.BackgroundColor.SetColor(0, 186, 248, 255);
+                sheet.Cells["A9:I9"].Style.Font.Bold = true;
+                sheet.Cells["A9:I9"].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                sheet.Cells["A9:I9"].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                sheet.Cells["A9:I9"].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                sheet.Cells["A9:I9"].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
                 sheet.Cells["A9:A9"].Value = "STT";
-                sheet.Cells["B9:B9"].Value = "Mã số SV";
-                sheet.Cells["C9:C9"].Value = "Họ và tên sinh viên";
-                sheet.Cells["D9:D9"].Value = "Ghi chú";
+                sheet.Cells["B9:B9"].Value = "Số hoá đơn";
+                sheet.Cells["C9:C9"].Value = "Ngày hoá đơn";
+                sheet.Cells["D9:D9"].Value = "Mã nhân viên";
+                sheet.Cells["E9:E9"].Value = "Tiền";
+                sheet.Cells["F9:F9"].Value = "Imei";
+                sheet.Cells["G9:G9"].Value = "Mã sản phẩm";
+                sheet.Cells["H9:H9"].Value = "Tên sản phẩm";
+                sheet.Cells["I9:I9"].Value = "Trạng thái";
 
-                var query = await (from t in lavenderContext.Sanpham
-                                   select t).ToListAsync();
-
-
+                var query = await (from x in lavenderContext.Hoadon
+                                   join y in lavenderContext.Chitiethoadon
+                                   on x.Sohoadon equals y.Sohoadon
+                                   join z in lavenderContext.Chitietsanpham
+                                   on y.Imei equals z.Imei
+                                   join t in lavenderContext.Sanpham
+                                   on z.Masanpham equals t.Masanpham
+                                   join k in lavenderContext.Vanchuyen
+                                   on x.Sohoadon equals k.Sohoadon
+                                   where x.Ngayhoadon <= DateTime.Parse(denngay)
+                                   && x.Ngayhoadon >= DateTime.Parse(tungay)
+                                   select new
+                                   {
+                                       sohoadon = x.Sohoadon,
+                                       ngayhoadon = x.Ngayhoadon,
+                                       manhanvien = x.Manhanvien,
+                                       tien = y.Tien,
+                                       imei = y.Imei,
+                                       masanpham = t.Masanpham,
+                                       tensanpham = t.Tensanpham,
+                                       trangthai = k.Trangthai
+                                   }).ToListAsync();
 
 
                 var count = query.Count();
-                if (count < 1)
-                {
-                    return Content("Không có sinh viên trong danh sách");
-                }
-
-
 
                 int i = 1;
                 int j = 10;
                 count = count + 10 - 1;
-                string chuoi = $"A10:D{count}";
+                string chuoi = $"A10:I{count}";
                 var query2 = query.ToList();
 
-
-
-                var malop_temp = query2[0].Masanpham;
-                if (count >= 10)
                 {
-                    sheet.Cells[chuoi].Style.Border.Left.Style = ExcelBorderStyle.Thin;
-                    sheet.Cells[chuoi].Style.Border.Right.Style = ExcelBorderStyle.Thin;
-                    sheet.Cells[chuoi].Style.Border.Top.Style = ExcelBorderStyle.Thin;
-                    sheet.Cells[chuoi].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
-                    sheet.Cells[chuoi].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                    sheet.Cells[chuoi].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                    if (count >= 10)
+                    {
+                        sheet.Cells[chuoi].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[chuoi].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[chuoi].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[chuoi].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[chuoi].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        sheet.Cells[chuoi].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                    }
+                    sheet.Cells["A5:A5"].Value = $"Từ ngày: {tungay}";
+                    sheet.Cells["A6:A6"].Value = $"Đến ngày: {denngay}";
                     foreach (var item in query)
                     {
                         sheet.Row(j).Height = 30;
                         string stt = $"A{j}:A{j}";
-                        string masv = $"B{j}:B{j}";
-                        string tensv = $"C{j}:C{j}";
+                        string sohoadon = $"B{j}:B{j}";
+                        string ngayhoadon = $"C{j}:C{j}";
+                        string manhanvien = $"D{j}:D{j}";
+                        string tien = $"E{j}:E{j}";
+                        string imei = $"F{j}:F{j}";
+                        string masanpham = $"G{j}:G{j}";
+                        string tensanpham = $"H{j}:H{j}";
+                        string trangthai = $"I{j}:I{j}";
+
+               
                         sheet.Cells[stt].Value = i;
-                        sheet.Cells["A5:C5"].Value = $"Khóa: {item.Tensanpham}";
-                        sheet.Cells["D5:E5"].Value = $"";
-                        sheet.Cells["A6:C6"].Value = $"Khoa: {item.Maloai}";
-                        sheet.Cells["D6:E6"].Value = $"Lớp: {item.Mathuonghieu}";
-                        sheet.Cells["A7:C7"].Value = $"Giảng viên CVHT: {item.Masanpham}";
-                        sheet.Cells["D7:E7"].Value = $"Mã giảng viên CVHT: {item.Masanpham}";
-                        sheet.Cells[masv].Value = item.Masanpham;
-                        sheet.Cells[tensv].Value = item.Masanpham;
+                        sheet.Cells[sohoadon].Value = item.sohoadon;
+                        sheet.Cells[ngayhoadon].Value = DateTime.Parse(item.ngayhoadon.ToString()).ToLocalTime().ToString();
+                        sheet.Cells[manhanvien].Value = item.manhanvien;
+                        sheet.Cells[tien].Value = item.tien;
+                        sheet.Cells[imei].Value = item.imei;
+                        sheet.Cells[masanpham].Value = item.masanpham;
+                        sheet.Cells[tensanpham].Value = item.tensanpham;
+                        sheet.Cells[trangthai].Value = item.trangthai;
                         i++;
                         j++;
                     }
@@ -154,10 +169,128 @@ namespace Back.Controllers
                 stream.Position = 0;
                 var tenfile = "Bao cao thong ke";
                 return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", tenfile);
-
             }
         }
 
+        [Route("/xuat-phieunhap")]
+        [Authorize(Roles = "ADMINISTRATOR, STAFF")]
+        public async Task<IActionResult> XuatPhieunhap(string tungay, string denngay)
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            var data = new DataTable();
+            var stream = new MemoryStream();
+            using (var package = new ExcelPackage(stream))
+            {
+                var sheet = package.Workbook.Worksheets.Add($"id");
+                sheet.Cells["A1:H99"].Style.Font.Name = "Times New Roman";
+                sheet.Cells["A1:C1"].Merge = true;
+                sheet.Column(3).Width = 25;
+                sheet.Column(1).Width = 5.33;
+                sheet.Column(2).Width = 11.67;
+                sheet.Column(5).Width = 20;
+                sheet.Cells["A1:H1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                sheet.Cells["A1:A1"].Value = "TRƯỜNG ĐH CÔNG NGHỆ THÔNG TIN";
+                sheet.Cells["A1:H1"].Merge = true;
 
+                sheet.Cells["A2:H2"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                sheet.Cells["A2:H2"].Style.Font.Bold = true;
+                sheet.Cells["A2:A2"].Value = "LAVENDER UIT WEBSHOP";
+                sheet.Cells["A2:H2"].Merge = true;
+
+                sheet.Cells["A4:H4"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                sheet.Cells["A4:H4"].Style.Font.Bold = true;
+                sheet.Cells["A4:H4"].Style.Font.Size = 16;
+                sheet.Cells["A4:A4"].Value = "THỐNG KÊ PHIẾU NHẬP SẢN PHẨM";
+                sheet.Cells["A4:H4"].Merge = true;
+
+                sheet.Cells["A5:H5"].Merge = true;
+                sheet.Cells["A5:H5"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                sheet.Cells["A5:H5"].Style.Font.Bold = true;
+                sheet.Cells["A5:A5"].Value = "Từ ngày:";
+
+                sheet.Cells["A6:H6"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                sheet.Cells["A6:H6"].Style.Font.Bold = true;
+                sheet.Cells["A6:A6"].Value = "Đến ngày:";
+                sheet.Cells["A6:H6"].Merge = true;
+
+                sheet.Row(9).Height = 41.13;
+                sheet.Cells["A9:H9"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                sheet.Cells["A9:H9"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                sheet.Cells["A9:H9"].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                sheet.Cells["A9:H9"].Style.Fill.BackgroundColor.SetColor(0, 186, 248, 255);
+                sheet.Cells["A9:H9"].Style.Font.Bold = true;
+                sheet.Cells["A9:H9"].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                sheet.Cells["A9:H9"].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                sheet.Cells["A9:H9"].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                sheet.Cells["A9:H9"].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                sheet.Cells["A9:A9"].Value = "STT";
+                sheet.Cells["B9:B9"].Value = "Mã phiếu nhập";
+                sheet.Cells["C9:C9"].Value = "Mã sản phẩm";
+                sheet.Cells["D9:D9"].Value = "Mã nhà cung cấp";
+                sheet.Cells["E9:E9"].Value = "Số lượng";
+                sheet.Cells["F9:F9"].Value = "Ngày nhập";
+                sheet.Cells["G9:G9"].Value = "Tiền nhập";
+                sheet.Cells["H9:H9"].Value = "Ghi chú";
+
+                var query = await (from x in lavenderContext.Phieunhapsanpham
+                                   where x.Ngaynhap <= DateTime.Parse(denngay)
+                                   && x.Ngaynhap >= DateTime.Parse(tungay)
+                                   select x).ToListAsync();
+
+
+                var count = query.Count();
+
+                int i = 1;
+                int j = 10;
+                count = count + 10 - 1;
+                string chuoi = $"A10:H{count}";
+                var query2 = query.ToList();
+
+                {
+                    if (count >= 10)
+                    {
+                        sheet.Cells[chuoi].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[chuoi].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[chuoi].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[chuoi].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[chuoi].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        sheet.Cells[chuoi].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                    }
+                    sheet.Cells["A5:A5"].Value = $"Từ ngày: {tungay}";
+                    sheet.Cells["A6:A6"].Value = $"Đến ngày: {denngay}";
+                    foreach (var item in query)
+                    {
+                        sheet.Row(j).Height = 30;
+                        string stt = $"A{j}:A{j}";
+                        string maphieunhap = $"B{j}:B{j}";
+                        string masanpham = $"C{j}:C{j}";
+                        string manhacungcap = $"D{j}:D{j}";
+                        string soluongnhap = $"E{j}:E{j}";
+                        string ngaynhap = $"F{j}:F{j}";
+                        string tiennhap = $"G{j}:G{j}";
+                        string ghichu = $"H{j}:H{j}";
+
+                  
+                        sheet.Cells[stt].Value = i;
+                        sheet.Cells[maphieunhap].Value = item.Maphieunhap;
+                        sheet.Cells[masanpham].Value = 
+                        sheet.Cells[manhacungcap].Value = item.Manhacungcap;
+                        sheet.Cells[soluongnhap].Value = item.Soluongnhap;
+                        sheet.Cells[ngaynhap].Value = DateTime.Parse(item.Ngaynhap.ToString()).ToLocalTime().ToString();
+                        sheet.Cells[tiennhap].Value = item.Tiennhap;
+                        sheet.Cells[ghichu].Value = item.Ghichu;
+                        i++;
+                        j++;
+                    }
+                    sheet.Name = "Bao cao thong ke";
+
+                    package.Save();
+                }
+
+                stream.Position = 0;
+                var tenfile = "Bao cao thong ke";
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", tenfile);
+            }
+        }
     }
 }
--- Back/Models/Sanpham.cs
+++ Back/Models/Sanpham.cs
@@ -27,14 +27,6 @@ namespace Back.Models
         public float Dongia { get; set; }  
         public int Thoigianbaohanh {get; set; }
 
-        public int Soluongton { get; set; }
-
-
-
-
-        public float Dongia { get; set; }  
-        public int Thoigianbaohanh {get; set; }
-
 
         public virtual Loaisanpham MaloaiNavigation { get; set; }
         public virtual Thuonghieu MathuonghieuNavigation { get; set; }
