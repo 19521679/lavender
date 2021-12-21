75925335b29a5111adb69c38a4f5b02124410239
19521679 <19521679@gm.uit.edu.vn>
2021-12-21 21:33:45 +0700

    deploy

--- lavender/src/Common/constants/index.js
+++ lavender/src/Common/constants/index.js
@@ -1,5 +1,5 @@
-//export const API_ENDPOINT = "https://localhost:5001";
- export const API_ENDPOINT = "https://lavender-uit-webshop.azurewebsites.net";
+export const API_ENDPOINT = "https://localhost:5001";
+//  export const API_ENDPOINT = "https://lavender-uit-webshop.azurewebsites.net";
 
 export const CLIENT_ID = "370364341020-hgdqap8bbmc406ubacgpiik8d7mkh4dt.apps.googleusercontent.com";
 export const CLIENT_ENDPOINT = "https://lavender-uit-webshop.web.app"
