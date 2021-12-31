2a854a4223207591e1225c47fc36d0376c9ca07f
chaupha180801 <chaupha180801@gmail.com>
2021-12-31 09:40:52 +0700

    Hello1

--- lavender/src/Common/constants/index.js
+++ lavender/src/Common/constants/index.js
@@ -1,7 +1,7 @@
-export const API_ENDPOINT = "https://localhost:5001";
-export const CLIENT_ENDPOINT = "http://locahost:3000"
+/* export const API_ENDPOINT = "https://localhost:5001";
+export const CLIENT_ENDPOINT = "http://locahost:3000" */
 
 
 export const CLIENT_ID = "370364341020-hgdqap8bbmc406ubacgpiik8d7mkh4dt.apps.googleusercontent.com";
-// export const CLIENT_ENDPOINT = "https://lavender-uit-webshop.web.app"
-// export const API_ENDPOINT = "https://lavender-uit-webshop.azurewebsites.net";
+export const CLIENT_ENDPOINT = "https://lavender-uit-webshop.web.app"
+export const API_ENDPOINT = "https://lavender-uit-webshop.azurewebsites.net";
